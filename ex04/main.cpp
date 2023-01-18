93598f2d117e97d6ae91946ea9f92223947995d5
sdxsd <willmaguire@icloud.com>
2023-01-18 15:32:59 +0100

    Nearly done with ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <fcntl.h>
 #include <bits/stdc++.h>
 
 int main(int argc, char *argv[]) {
 	std::string			fileName;
 	std::string			data;
 	std::fstream		src;
-	std::fstream		dst;
+	std::ofstream		dst;
 
+	if (argc < 4 || argc > 4)
+		return (0);
 	fileName = argv[1];
 	src.open(fileName.c_str());
 	dst.open(fileName.append(".replace").c_str());
@@ -16,5 +19,8 @@ int main(int argc, char *argv[]) {
 		else
 			dst << data;
 	}
+	dst << std::endl;
+	dst.close();
+	src.close();
 	return (0);
 }
