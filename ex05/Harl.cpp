055472224f9672b7685ae71729d2c680ee04f0d8
sdxsd <willmaguire@icloud.com>
2023-01-18 16:43:45 +0100

    Harl is a bastard.

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -1,5 +1,34 @@
 #include "Harl.hpp"
 
-void Harl::complain(std::string level) {
+void Harl::debug(void) {
 
+}
+
+void Harl::info(void) {
+
+}
+
+void Harl::warning(void) {
+
+}
+
+void Harl::error(void) {
+
+}
+
+Harl::Harl(void) {
+	messageLevels[0].levelName = "debug";
+	messageLevels[0].levelFunction = &Harl::debug;
+	messageLevels[1].levelName = "info";
+	messageLevels[1].levelFunction = &Harl::info;
+	messageLevels[2].levelName = "warning";
+	messageLevels[2].levelFunction = &Harl::warning;
+	messageLevels[3].levelName = "error";
+	messageLevels[3].levelFunction = &Harl::error;
+}
+
+void Harl::complain(std::string level) {
+	for (int i = 0; i < sizeof(messageLevels); i++)
+		if (messageLevels[i].levelName == level)
+			messageLevels[i].levelFunction();
 }
--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -3,13 +3,23 @@
 
 #include <iostream>
 
+class Harl;
+
+typedef struct s_level {
+	std::string	levelName;
+	void		(Harl::*levelFunction)();
+} t_level;
+
 class Harl {
 	private:
+		t_level	messageLevels[4];
 		void	debug(void);
 		void	info(void);
 		void	warning(void);
 		void	error(void);
 	public:
+		Harl(void);
+		~Harl(void);
 		void	complain(std::string level);
 };
 
