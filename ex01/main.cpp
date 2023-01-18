d3b789276d1b7d3ca44ba8676eae4e4fdba96ed9
sdxsd <willmaguire@icloud.com>
2023-01-18 12:13:54 +0100

    Quick SEGFAULT fix.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,10 +4,10 @@ int main(int argc, char *argv[]) {
 	Zombie	*horde;
 	int		n_zombies;
 
-	n_zombies = atoi(argv[1]);
-	if (n_zombies == 0)
-		return (0);
 	if (argc > 1 && argc <= 3) {
+		n_zombies = atoi(argv[1]);
+		if (n_zombies == 0)
+			return (0);
 		horde = zombieHorde(n_zombies, argv[2]);
 		for (int i = 0; i < n_zombies; i++)
 			horde[i].announce();
