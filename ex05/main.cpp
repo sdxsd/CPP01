71a465f2284bd98a7d47d82db029dd729415ce07
sdxsd <willmaguire@icloud.com>
2023-01-23 12:46:08 +0100

    Did I not initially add this?

---
+++ ex05/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Harl.hpp"
+
+int main(void) {
+	Harl karen;
+
+	karen.complain("debug");
+	karen.complain("info");
+	karen.complain("warning");
+	karen.complain("error");
+	return (0);
+}
