19315812f6c4826188b2d48f3914a26ce98c1df5
sdxsd <willmaguire@icloud.com>
2023-01-31 13:08:12 +0100

    Error checking is always good.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,8 +7,10 @@ int main(int argc, char *argv[]) {
 
 	if (argc > 1 && argc <= 3) {
 		n_zombies = atoi(argv[1]);
-		if (n_zombies == 0)
+		if (n_zombies == 0) {
+			std::cout << "Invalid input." << std::endl;
 			return (0);
+		}
 		horde = zombieHorde(n_zombies, argv[2]);
 		for (int i = 0; i < n_zombies; i++)
 			horde[i].announce();
